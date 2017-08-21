9ea217b7f36b9eb28744522a07d184315a164c1d
John Garbutt <john@johngarbutt.com>
2017-08-21 17:28:24 +0100

    Add flavors into capacity prototype

--- os_capacity/keystone.py
+++ os_capacity/keystone.py
@@ -13,15 +13,13 @@
 # under the License.
 
 import argparse
+import pprint
 import sys
 
 from keystoneauth1 import loading as ks_loading
 
-from keystoneauth1.identity import v3
-from keystoneclient.v3 import client
 
-
-def list_rps(argv):
+def get_client(argv):
     parser = argparse.ArgumentParser('os-capacity')
     ks_loading.register_session_argparse_arguments(parser)
     ks_loading.register_auth_argparse_arguments(parser, argv)
@@ -31,13 +29,38 @@ def list_rps(argv):
     auth = ks_loading.load_auth_from_argparse_arguments(args)
     client = ks_loading.load_session_from_argparse_arguments(args, auth=auth)
 
+    return client
+
+
+def print_flavors(client):
+    ks_filter = {'service_type': 'compute',
+                 'region_name': 'RegionOne',
+                 'interface': 'public'}
+    url_list_providers = "/flavors" 
+    response = client.get(
+            url_list_providers,
+            endpoint_filter=ks_filter).json()
+    print
+    print "Flavors:"
+    print
+    pprint.pprint(response['flavors'])
+
+
+def print_rps(client):
     ks_filter = {'service_type': 'placement',
                  'region_name': 'RegionOne',
                  'interface': 'public'}
-    url = "/resource_providers" 
-    print client.get(
-            url,
+    url_list_providers = "/resource_providers" 
+    response = client.get(
+            url_list_providers,
             endpoint_filter=ks_filter).json()
+    print
+    print "Resource Providers:"
+    print
+    pprint.pprint(response['resource_providers'])
+
 
 if __name__ == "__main__":
-    list_rps(sys.argv)
+    client = get_client(sys.argv)
+    print_flavors(client)
+    print_rps(client)
