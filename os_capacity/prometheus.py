e3b9d66c43002857ae2cc4922d692b56367608e2
John Garbutt <john.garbutt@stackhpc.com>
2023-01-23 22:58:01 +0000

    Add AZs into capacity info

--- os_capacity/prometheus.py
+++ os_capacity/prometheus.py
@@ -65,6 +65,49 @@ def get_max_per_host(placement_client, resources, required_traits):
     return count_per_rp
 
 
+def get_resource_provider_info(compute_client, placement_client):
+    # get host aggregates to look up things like az
+    nova_aggregates = list(compute_client.aggregates())
+
+    azones = {}
+    project_filters = {}
+    for agg in nova_aggregates:
+        az = agg.metadata.get("availability_zone")
+        if az:
+            azones[agg.uuid] = az
+
+        projects = []
+        for key in agg.metadata.keys():
+            if key.startswith("filter_tenant_id"):
+                projects.append(agg.metadata[key])
+        if projects:
+            # TODO(johngarbutt): expose project id to aggregate names?
+            project_filters[agg.uuid] = {"name": agg.name, "projects": projects}
+
+    # print(json.dumps(azones, indent=2))
+    # print(json.dumps(project_filters, indent=2))
+
+    raw_rps = list(placement_client.resource_providers())
+
+    resource_providers = {}
+    for raw_rp in raw_rps:
+        rp = {"uuid": raw_rp.id}
+        resource_providers[raw_rp.name] = rp
+        # TODO - get aggregates
+        response = placement_client.get(
+            f"/resource_providers/{raw_rp.id}/aggregates",
+            headers={"OpenStack-API-Version": "placement 1.19"},
+        )
+        response.raise_for_status()
+        aggs = response.json()
+        rp["aggregates"] = aggs["aggregates"]
+        for agg_id in rp["aggregates"]:
+            if agg_id in azones:
+                rp["az"] = azones[agg_id]
+            if agg_id in project_filters:
+                rp["project_filter"] = project_filters[agg_id]["name"]
+    return resource_providers
+
 
 def print_details(compute_client, placement_client):
     flavors = list(compute_client.flavors())
@@ -78,17 +121,23 @@ def print_details(compute_client, placement_client):
         print(f'openstack_total_capacity_per_flavor{{flavor="{flavor_name}"}} {total}')
 
     # capacity per host
-    raw_rps = list(placement_client.resource_providers())
-    resource_providers = {rp.name: rp.id for rp in raw_rps}
+    resource_providers = get_resource_provider_info(compute_client, placement_client)
     hostnames = sorted(resource_providers.keys())
     for hostname in hostnames:
-        rp_id = resource_providers[hostname]
+        rp = resource_providers[hostname]
+        rp_id = rp["uuid"]
         for flavor_name in flavor_names:
             all_counts = capacity_per_flavor.get(flavor_name, {})
             our_count = all_counts.get(rp_id, 0)
             if our_count == 0:
                 continue
             host_str = f'hypervisor="{hostname}"'
+            az = rp.get("az")
+            if az:
+                host_str += f',az="{az}"'
+            project_filter = rp.get("project_filter")
+            if project_filter:
+                host_str += f',project_filter="{project_filter}"'
             print(
                 f'openstack_capacity_by_hostname{{{host_str},flavor="{flavor_name}"}} {our_count}'
             )
@@ -98,6 +147,6 @@ def print_exporter_data(app):
     print_details(app.compute_client, app.placement_client)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     conn = openstack.connect()
     print_details(conn.compute, conn.placement)
